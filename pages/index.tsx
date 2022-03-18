464a3f03edbb2178f5e350cca5b638b38040d329
Rafael Henrique Berro <rhberro@gmail.com>
2022-03-17 22:02:08 -0300

    Trying to fex prerender problems

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
+import config from "../next-i18next.config";
 
 function Home(): JSX.Element {
   const { t } = useTranslation("common");
@@ -19,7 +20,7 @@ function Home(): JSX.Element {
 
 export const getStaticProps = async ({ locale }: any) => ({
   props: {
-    ...(await serverSideTranslations(locale, ["common"])),
+    ...(await serverSideTranslations(locale, ["common"], config)),
   },
 });
 
